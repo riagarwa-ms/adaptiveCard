beb59b40e32d803dbd721352b30b9b36f5a910d8
Rishika Agarwal <riagarwa@microsoft.com>
2021-01-15 15:03:43 +0530

    updating logic to fetch cert

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -24,32 +24,43 @@ using AdaptiveCards;
 using System.IO;
 using AdaptiveCards.Templating;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace HelloWorldBot.Bots
 {
     public class EchoBot : TeamsActivityHandler
     {
-        private const string tokenRequestUrl = "https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/oauth2/v2.0/token";
-        private const string aud = "https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0";
+        private const string aadLoginUrl = "https://login.microsoftonline.com/";
+        private const string tokenRequestParam = "/oauth2/v2.0/token";
+        private const string firstPartyAppID = "e5e15768-1702-474d-ba7b-904c7cad2bcf";
+        private const string clientIdKey = "client_id";
         private const string clientId = "e5e15768-1702-474d-ba7b-904c7cad2bcf";
-        private const string clientAssertionType = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
-        private const string clientCredentials = "client_credentials";
-        private const string authenticationKey = "authentication";
-        private const string tokenKey = "token";
-        private const string urlKey = "url";
+        private const string clientAssertionKey = "client_assertion";
+        private const string clientAssertionTypeKey = "client_assertion_type";
+        private const string clientAssertionTypeValue = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
+        private const string clientCredentialsKey = "grant_type";
+        private const string clientCredentialsValue = "client_credentials";
+        private const string scopeKey = "scope";
         private const string authorizationKey = "Authorization";
         private const string acceptKey = "Accept";
         private const string acceptJsonVal = "application/json";
+        private string tenantId = "";
 
         protected override async Task<MessagingExtensionResponse> OnTeamsAppBasedLinkQueryAsync(ITurnContext<IInvokeActivity> turnContext, AppBasedLinkQuery query, CancellationToken cancellationToken)
         {
+            JObject tenant = JObject.FromObject(turnContext.Activity.ChannelData.tenant);
+            if (tenant != null && tenant["id"] != null)
+            {
+                tenantId = tenant["id"].ToString();
+            }
+
             if (turnContext != null && turnContext.Activity != null)
             {
                 JObject valueObject = JObject.FromObject(turnContext.Activity.Value);
                 if (valueObject["authentication"] != null)
                 {
-                    string accessToken = TransformPFTToken((valueObject[authenticationKey][tokenKey]).ToString());
-                    Uri inputUrl = new Uri(valueObject[urlKey].ToString());
+                    string accessToken = TransformPFTToken((valueObject["authentication"]["token"]).ToString());
+                    Uri inputUrl = new Uri(valueObject["url"].ToString());
 
                     string actorToken = await GetActorToken(inputUrl);
                     JObject[] spMetadata = await GetSharePointMetadata(accessToken, actorToken, inputUrl);
@@ -73,7 +84,8 @@ namespace HelloWorldBot.Bots
         /*
          * Modufy the header in the PFT token so that it can be accepted by SharePoint service
          * Documentation: https://aadwiki.windows-int.net/index.php?title=Server-to-server_authentication
-         */
+         * */
+
         private string TransformPFTToken(String preTransformedPFTToken)
         {
             JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(preTransformedPFTToken);
@@ -82,7 +94,7 @@ namespace HelloWorldBot.Bots
             string nonceIn = headerObject.GetValue("nonce", StringComparison.OrdinalIgnoreCase).ToString();
             string hashName = headerObject.GetValue("alg", StringComparison.OrdinalIgnoreCase).ToString();
 
-            HashAlgorithm hashAlgorithm = EchoBot.GetHashFunction(hashName);
+            HashAlgorithm hashAlgorithm = GetHashFunction(hashName);
             string nonceOut = Base64UrlEncoder.Encode(hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(nonceIn)));
             headerObject["nonce"] = nonceOut;
             string newHeaderString = Base64UrlEncoder.Encode(headerObject.ToString(Formatting.None));
@@ -90,7 +102,7 @@ namespace HelloWorldBot.Bots
             return postTransformedToken;
         }
 
-        private static HashAlgorithm GetHashFunction(string hashAlgorithmName)
+        private HashAlgorithm GetHashFunction(string hashAlgorithmName)
         {
             switch (hashAlgorithmName)
             {
@@ -106,19 +118,20 @@ namespace HelloWorldBot.Bots
         {
             try
             {
-                string scope = spUrl.Scheme + "://" + spUrl.Host + "/.default";
+                string scope = $"{spUrl.Scheme}://{spUrl.Host}/.default";
+                string tokenRequestUrl = $"{aadLoginUrl}{tenantId}{tokenRequestParam}";
 
                 HttpClient client = new HttpClient();
                 client.BaseAddress = new Uri(tokenRequestUrl);
-                string clientAssertion = GetSignedClientAssertion();
+                string clientAssertionValue = GetSignedClientAssertion();
 
                 Dictionary<string, string> values = new Dictionary<string, string>
                 {
-                    { "grant_type", clientCredentials },
-                    { "client_id", clientId },
-                    {"client_assertion", clientAssertion },
-                    {"scope", scope },
-                    {"client_assertion_type", clientAssertionType }
+                    { clientCredentialsKey, clientCredentialsValue },
+                    { clientIdKey, clientId },
+                    {clientAssertionKey, clientAssertionValue },
+                    {scopeKey, scope },
+                    {clientAssertionTypeKey, clientAssertionTypeValue }
                 };
 
                 client.DefaultRequestHeaders.Add(acceptKey, acceptJsonVal);
@@ -135,7 +148,7 @@ namespace HelloWorldBot.Bots
                     }
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 throw e;
             }
@@ -147,10 +160,27 @@ namespace HelloWorldBot.Bots
          * Create self signed certificate using Azure Key Vault
          * The code in this method currently uses the local pfx file, later on this will be modified
          */
+
         private string GetSignedClientAssertion()
         {
-            X509Certificate2 selfSignedCertificate = new X509Certificate2(@"C:\Users\riagarwa\Downloads\oct5keyvault-ProdCert2-20201211.pfx", "", X509KeyStorageFlags.EphemeralKeySet);
-            Dictionary<string, object> claims = new Dictionary<string, object>()
+            string certThumbprint = "89F68251505D05923994B05B25AEB9E9954C2F68";
+            bool validOnly = false;
+
+            using (X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                certStore.Open(OpenFlags.ReadOnly);
+
+                X509Certificate2Collection certCollection = certStore.Certificates.Find(
+                                            X509FindType.FindByThumbprint,
+                                            // Replace below with your certificate's thumbprint
+                                            certThumbprint,
+                                            validOnly);
+                // Get the first cert with the thumbprint
+                X509Certificate2 selfSignedCertificate = certCollection.OfType<X509Certificate2>().FirstOrDefault();
+
+                string aud = $"{aadLoginUrl}{tenantId}/v2.0";
+
+                Dictionary<string, object> claims = new Dictionary<string, object>()
             {
                 { "aud", aud },
                 { "iss", clientId },
@@ -158,64 +188,65 @@ namespace HelloWorldBot.Bots
                 { "sub", clientId }
             };
 
-            SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor
-            {
-                Claims = claims,
-                SigningCredentials = new X509SigningCredentials(selfSignedCertificate)
-            };
+                SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Claims = claims,
+                    SigningCredentials = new X509SigningCredentials(selfSignedCertificate)
+                };
 
-            JsonWebTokenHandler handler = new JsonWebTokenHandler();
-            string signedClientAssertion = handler.CreateToken(securityTokenDescriptor);
-            return signedClientAssertion;
+                JsonWebTokenHandler handler = new JsonWebTokenHandler();
+                string signedClientAssertion = handler.CreateToken(securityTokenDescriptor);
+                return signedClientAssertion;
+            }
         }
 
         private async Task<JObject[]> GetSharePointMetadata(string accessToken, string actorToken, Uri spUrl)
         {
             var taskList = new[]
             {
-                EchoBot.GetSiteContent(accessToken, actorToken, spUrl),
-                EchoBot.GetSiteTitle(accessToken, actorToken, spUrl),
-                EchoBot.GetImageThumbnail(accessToken, actorToken, spUrl)
+                GetSiteContent(accessToken, actorToken, spUrl),
+                GetSiteTitle(accessToken, actorToken, spUrl),
+                //EchoBot.GetImageThumbnail(accessToken, actorToken, spUrl)
             };
 
             return await Task.WhenAll(taskList);
         }
 
-        private static async Task<JObject> GetSiteTitle(string accessToken, string actorToken, Uri url)
+        private async Task<JObject> GetSiteTitle(string accessToken, string actorToken, Uri url)
         {
-            string teamSite = Regex.Split(url.ToString(), @"/\/ sitepages\//i")[0];
+            string teamSite = Regex.Split(url.ToString(), @"/sitepages/", RegexOptions.IgnoreCase)[0];
             string requestUrl = teamSite + "/_api/web/Title";
 
             HttpResponseMessage responseMessage = await MakePFTRequest(accessToken, actorToken, requestUrl);
             if (responseMessage.IsSuccessStatusCode)
             {
                 string responseStr = await responseMessage.Content.ReadAsStringAsync();
-                return JObject.Parse(responseStr);
+                return JsonConvert.DeserializeObject<JObject>(responseStr);
             }
             return null;
         }
 
-        private static async Task<JObject> GetSiteContent(string accessToken, string actorToken, Uri url)
+        private async Task<JObject> GetSiteContent(string accessToken, string actorToken, Uri url)
         {
-            string teamSite = Regex.Split(url.ToString(), @"/\/ sitepages\//i")[0];
-            string requestUrl = teamSite + "/_api/sitepages/pages/GetByUrl('" + url.AbsolutePath + "')";
+            string teamSite = Regex.Split(url.ToString(), @"/sitepages/", RegexOptions.IgnoreCase)[0];
+            string requestUrl = $"{teamSite}/_api/sitepages/pages/GetByUrl('{url.AbsolutePath}')";
 
             HttpResponseMessage responseMessage = await MakePFTRequest(accessToken, actorToken, requestUrl);
             if (responseMessage.IsSuccessStatusCode)
             {
                 string responseStr = await responseMessage.Content.ReadAsStringAsync();
-                return JObject.Parse(responseStr);
+                return JsonConvert.DeserializeObject<JObject>(responseStr);
             }
             return null;
         }
 
-        private static async Task<JObject> GetImageThumbnail(string accessToken, string actorToken, Uri url)
+        private async Task<JObject> GetImageThumbnail(string accessToken, string actorToken, Uri url)
         {
-            string teamSite = Regex.Split(url.ToString(), @"/\/ sitepages\//i")[0];
-            string requestUrl = url.Scheme + "://" + url.Host + "/_api/v2.0/sharePoint:/" + teamSite + ":/driveItem/thumbnails/0/c71x40/content";
+            string teamSite = Regex.Split(url.ToString(), @"/\/sitepages\//i", RegexOptions.IgnoreCase)[0];
+            string requestUrl = $"{ url.Scheme}://{url.Host}/_api/v2.0/sharePoint:/{teamSite}:/driveItem/thumbnails/0/c71x40/content";
 
             HttpResponseMessage responseMessage = await MakePFTRequest(accessToken, actorToken, requestUrl);
-            if(responseMessage.IsSuccessStatusCode)
+            if (responseMessage.IsSuccessStatusCode)
             {
                 byte[] responseStr = await responseMessage.Content.ReadAsByteArrayAsync();
                 JObject obj = new JObject();
@@ -225,10 +256,10 @@ namespace HelloWorldBot.Bots
             return null;
         }
 
-        private static async Task<HttpResponseMessage> MakePFTRequest(string accessToken, string actorToken, string apiUrl)
+        private async Task<HttpResponseMessage> MakePFTRequest(string accessToken, string actorToken, string apiUrl)
         {
             HttpClient client = new HttpClient();
-            string authorizationHeaderValue = "MSAuth1.0 actortoken=" + '"' + 
+            string authorizationHeaderValue = "MSAuth1.0 actortoken=" + '"' +
                 "Bearer " + actorToken + '"' + ", accesstoken=" + '"' + "Bearer " + accessToken + '"' + ", type=" + '"' + "PFAT" + '"';
 
             client.DefaultRequestHeaders.Add(authorizationKey, authorizationHeaderValue);
@@ -237,21 +268,21 @@ namespace HelloWorldBot.Bots
             return await client.GetAsync(apiUrl);
         }
 
-        private static MessagingExtensionResponse CreateAdaptiveCard(JObject[] spMetadata)
+        private MessagingExtensionResponse CreateAdaptiveCard(JObject[] spMetadata)
         {
             try
             {
-                string cardTemplate = Path.Combine(".", "Resources", "adaptiveCardSample.json");
+                string cardTemplate = Path.Combine(".", "adaptiveCardSample.json");
                 string cardContent = File.ReadAllText(cardTemplate);
                 AdaptiveCardTemplate template = new AdaptiveCardTemplate(cardContent);
 
                 var spData = new
                 {
-                    imageUrl = spMetadata[2]["imageUrlInBase64"].ToString(),
+                    imageUrl = spMetadata[0]["BannerImageUrl"].ToString(),
                     siteName = spMetadata[0]["Title"].ToString(),
                     pageTitle = spMetadata[1]["value"].ToString(),
-                    authorName = spMetadata[0]["Author"].ToString(),
-                    authorDate = spMetadata[0]["Date"].ToString()
+                    authorName = GetAuthorName(spMetadata[0]),
+                    authorDate = spMetadata[0]["FirstPublished"].ToString()
                 };
 
                 string cardJson = template.Expand(spData);
@@ -274,7 +305,7 @@ namespace HelloWorldBot.Bots
             }
         }
 
-        private static MessagingExtensionAttachment CreateAdaptiveCardAttachment(string cardJson, HeroCard previewCard)
+        private MessagingExtensionAttachment CreateAdaptiveCardAttachment(string cardJson, HeroCard previewCard)
         {
             MessagingExtensionAttachment adaptiveCardAttachment = new MessagingExtensionAttachment()
             {
@@ -284,6 +315,29 @@ namespace HelloWorldBot.Bots
             };
 
             return adaptiveCardAttachment;
+        }
+
+        private String GetAuthorName(JObject spPageContent)
+        {
+            string authorName = "";
+            try
+            {
+                if (spPageContent != null && spPageContent["LayoutWebpartsContent"] != null)
+                {
+                    JArray layoutwebpartsContentStr = JsonConvert.DeserializeObject<JArray>(spPageContent["LayoutWebpartsContent"].ToString());
+                    if (layoutwebpartsContentStr != null && layoutwebpartsContentStr[0] != null && layoutwebpartsContentStr[0]["properties"] != null &&
+                        layoutwebpartsContentStr[0]["properties"]["authors"] != null && layoutwebpartsContentStr[0]["properties"]["authors"][0] != null
+                        && layoutwebpartsContentStr[0]["properties"]["authors"][0]["name"] != null)
+                    {
+                        authorName = layoutwebpartsContentStr[0]["properties"]["authors"][0]["name"].ToString();
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                return authorName;
+            }
+            return authorName;
         }
     }
 }
